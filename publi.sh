e8e7a8f5c5da20e62a1a2f216bc7685af089ed50
Sergio Gutierrez-Santos "Sergut <sergut@gmail>
2012-10-17 02:44:00 +0100

    Added small script for publishing notes every day.

---
+++ publi.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+usage() {
+  echo "publi.sh <filenamewithoutextension>"
+  exit 0
+}
+
+fileNotFound() {
+  echo "File $1 does not exist. Nothing to do..."
+  exit 0
+}
+
+[ -n "$1" ] && FILE=$1 || usage
+[ -e "$FILE".tex ] || fileNotFound $FILE.tex
+
+latex $FILE && \
+   latex $FILE && \
+   latex $FILE && \
+   dvips $FILE.dvi -o && \
+   ps2pdf $FILE.ps && \
+   echo && \
+   latex $FILE |grep "LaTeX Warning" |cat && \
+   echo && \
+   echo $FILE.pdf created...
