01e81982111a9ee3341a236657d2a2392151e000
Sergio Gutierrez-Santos "Sergut <sergut@gmail>
2012-12-11 16:27:19 +0000

    Fixed a bug detected by A. Rokotas.
    From: A. Rokotas
    To: sergut
    
    Hello,
    
    The url is :
    http://www.thecareersgroup.co.uk/contact-us/college-careers-services.aspx
    
    The output is : [7137careers@gold.ac.uk,
    6030c2education@careers.lon.ac.uk, 7134careers@kcl.ac.u
    k, 5065careers@qmul.ac.uk, 443073careers@rhul.ac.uk,
    4055careers@smuc.ac.uk, 411
    5careers@soas.ac.uk, 3600careers@ucl.ac.uk]

--- src/WebAnalyzer.java
+++ src/WebAnalyzer.java
@@ -32,185 +32,181 @@ import java.util.regex.Pattern;
  */
 public final class WebAnalyzer
 {
-	/**
-         * A regular expression to identify email addresses
-         */
-	private final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    /**
+     * A regular expression to identify email addresses
+     */
+    private final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-	/**
-         * The URL of the page
-         */
-	private URL url;
+    /**
+     * The URL of the page
+     */
+    private URL url;
 
-        /**
-         * A representation of the HTML code of the page
-         */
-	private HTMLDocument document;
-	
-	/**
-         * A new WebAnalyzer.
-         * 
-         * @param url the URL of the page to analyze
-         * 
-         * @throws MalformedURLException if the URL is not well formed
-         * @throws BadLocationException if there are problems at the back-end
-         * @throws IOException if there is other kind I/O problems
-         */
-	public WebAnalyzer(String url) throws MalformedURLException,IOException, BadLocationException 
-	{
-		this.url=new URL(url);
-		document=getHTMLDocument();
-	}
-	
-	private HTMLDocument getHTMLDocument() throws IOException, BadLocationException
-	{
-		HTMLEditorKit kit=new HTMLEditorKit(); 
-		HTMLDocument document=(HTMLDocument) kit.createDefaultDocument(); 
-		document.putProperty("IgnoreCharsetDirective", Boolean.TRUE);
-		Reader reader=new InputStreamReader(url.openStream()); 
-		kit.read(reader, document, 0);
-		
-		return document;		
-	}
+    /**
+     * A representation of the HTML code of the page
+     */
+    private HTMLDocument document;
+      
+    /**
+     * A new WebAnalyzer.
+     * 
+     * @param url the URL of the page to analyze
+     * 
+     * @throws MalformedURLException if the URL is not well formed
+     * @throws BadLocationException if there are problems at the back-end
+     * @throws IOException if there is other kind I/O problems
+     */
+    public WebAnalyzer(String url) throws MalformedURLException,IOException, BadLocationException 
+    {
+        this.url=new URL(url);
+        document=getHTMLDocument();
+    }
+      
+    private HTMLDocument getHTMLDocument() throws IOException, BadLocationException
+    {
+        HTMLEditorKit kit=new HTMLEditorKit(); 
+        HTMLDocument document=(HTMLDocument) kit.createDefaultDocument(); 
+        document.putProperty("IgnoreCharsetDirective", Boolean.TRUE);
+        Reader reader=new InputStreamReader(url.openStream()); 
+        kit.read(reader, document, 0);
+            
+        return document;            
+    }
 
-        /**
-         * Returns the links pointing to other pages. 
-         *	
-         * @return the links pointing to other pages. 
-         *	
-         */
-	public Set<String> getLinks()
-	{
-		Set<String> result=new HashSet<String>();
-	
-		for (HTMLDocument.Iterator iterator=document.getIterator(Tag.A);iterator.isValid();iterator.next())
-		{
-			AttributeSet attributes=iterator.getAttributes();
-			String href=(String)attributes.getAttribute(Attribute.HREF);
-			href=buildAbsoluteURL(href);
-			
-			if(!href.equals("") && !href.startsWith("javascript"))
-			{
-				result.add(href);
-			}
-		}
+    /**
+     * Returns the links pointing to other pages. 
+     *      
+     * @return the links pointing to other pages. 
+     *      
+     */
+    public Set<String> getLinks()
+    {
+        Set<String> result=new HashSet<String>();
+      
+        for (HTMLDocument.Iterator iterator=document.getIterator(Tag.A);iterator.isValid();iterator.next())
+            {
+                AttributeSet attributes=iterator.getAttributes();
+                String href=(String)attributes.getAttribute(Attribute.HREF);
+                href=buildAbsoluteURL(href);
+                  
+                if(!href.equals("") && !href.startsWith("javascript"))
+                    {
+                        result.add(href);
+                    }
+            }
 
-		return result;
-	}
-	
-        /**
-         * Returns the email addresses in this page
-         *	
-         * @return the email addresses in this page
-         *	
-         */
-	public Set<String> getEmails()
-	{
-		Set<String> result=new HashSet<String>();
+        return result;
+    }
+      
+    /**
+     * Returns the email addresses in this page
+     *      
+     * @return the email addresses in this page
+     *      
+     */
+    public Set<String> getEmails()
+    {
+        Set<String> result=new HashSet<String>();
 
-		ElementIterator iterator=new ElementIterator(document); 
-		Element element; 
+        ElementIterator iterator=new ElementIterator(document); 
+        Element element; 
     
-		while((element=iterator.next())!=null)
-		{
-			AttributeSet attributes=element.getAttributes();
-			Object name=attributes.getAttribute(StyleConstants.NameAttribute);
+        while((element=iterator.next())!=null)
+            {
+                AttributeSet attributes=element.getAttributes();
+                Object name=attributes.getAttribute(StyleConstants.NameAttribute);
 
-			if (name instanceof Tag)
-			{
-				StringBuffer buffer=new StringBuffer();
-				int count=element.getElementCount();
+                if (name instanceof Tag)
+                    {
+                        StringBuffer buffer=new StringBuffer();
+                        int count=element.getElementCount();
         
-				for (int i=0; i<count; i++)
-				{
-					Element child=element.getElement(i);
-					AttributeSet childAttributes=child.getAttributes();
+                        for (int i=0; i<count; i++)
+                            {
+                                Element child=element.getElement(i);
+                                AttributeSet childAttributes=child.getAttributes();
 
-					if (childAttributes.getAttribute(StyleConstants.NameAttribute)==Tag.CONTENT)
-					{
-						int startOffset = child.getStartOffset();
-						int endOffset = child.getEndOffset();
-						int length = endOffset - startOffset;
-						
-						try
-						{
-							buffer.append(document.getText(startOffset, length));
-						}
-						catch(BadLocationException e)
-						{
-							e.printStackTrace();
-						}
-					}
-				}
+                                if (childAttributes.getAttribute(StyleConstants.NameAttribute)==Tag.CONTENT)
+                                    {
+                                        int startOffset = child.getStartOffset();
+                                        int endOffset = child.getEndOffset();
+                                        int length = endOffset - startOffset;
+                                        try
+                                            {
+                                                buffer.append(" " + document.getText(startOffset, length));
+                                            }
+                                        catch(BadLocationException e)
+                                            {
+                                                e.printStackTrace();
+                                            }
+                                    }
+                            }
 
-				Pattern pattern=Pattern.compile(EMAIL_PATTERN);
-				StringTokenizer tokenizer=new StringTokenizer(buffer.toString());
-		
-				while(tokenizer.hasMoreTokens())
-				{
-					String token=tokenizer.nextToken();
-					Matcher matcher=pattern.matcher(token);
-			
-					if(matcher.matches())
-					{
-						result.add(token);
-					}
-				}
-			}
-		}
+                        Pattern pattern=Pattern.compile(EMAIL_PATTERN);
+                        StringTokenizer tokenizer=new StringTokenizer(buffer.toString());
+            
+                        while(tokenizer.hasMoreTokens())
+                            {
+                                String token=tokenizer.nextToken();
+                                Matcher matcher=pattern.matcher(token);             
+                                if(matcher.matches())
+                                    {
+                                        result.add(token);
+                                    }
+                            }
+                    }
+            }
 
-		return result;
-	}
-	
-	private String buildAbsoluteURL(String href)
-	{
-		URI uri=null;
-		
-		try
-		{
-			uri=new URI(href);
-		}
-		catch(URISyntaxException e)
-		{
-			System.err.println("URI syntax problem: " + href);
-			return "";
-		}
-		catch(Exception e)
-		{
-			System.err.println("URI problem: " + href);
-			return "";
-		}
-		
-		if(uri.isAbsolute())
-		{
-			return href;
-		}
-	
-		String newUrl="";
-		newUrl+=url.getProtocol()+"://";	
-		newUrl+=url.getAuthority();
+        return result;
+    }
+      
+    private String buildAbsoluteURL(String href)
+    {
+        URI uri=null;
+        try
+            {
+                uri=new URI(href);
+            }
+        catch(URISyntaxException e)
+            {
+                System.err.println("URI syntax problem: " + href);
+                return "";
+            }
+        catch(Exception e)
+            {
+                System.err.println("URI problem: " + href);
+                return "";
+            }
+            
+        if(uri.isAbsolute())
+            {
+                return href;
+            }
+        String newUrl="";
+        newUrl+=url.getProtocol()+"://";  
+        newUrl+=url.getAuthority();
+        String path=url.getPath();
+        int position=path.lastIndexOf('/')+1;
+        path=path.substring(0,position);
+        if(path.equals(""))
+            {
+                newUrl+="/";
+            }
+        else
+            {
+                newUrl+=path;
+            }
+            
+        return newUrl+href;
+    }
 
-		String path=url.getPath();
-		int position=path.lastIndexOf('/')+1;
-		path=path.substring(0,position);
-		
-		if(path.equals(""))
-		{
-			newUrl+="/";
-		}
-		else
-		{
-			newUrl+=path;
-		}
-		
-		return newUrl+href;
-	}
-
-	public static void main(String args[]) throws Exception {
-		WebAnalyzer wa = new WebAnalyzer("http://www.bbk.ac.uk");
-                for (String s : wa.getEmails()) 
- 			System.out.println("email: " + s);
-                for (String s : wa.getLinks()) 
- 			System.out.println("Link: " + s);
+    public static void main(String args[]) throws Exception {
+        WebAnalyzer wa = new WebAnalyzer("http://www.bbk.ac.uk");
+        for (String s : wa.getEmails()) {
+            System.out.println("email: " + s);
         }
+        for (String s : wa.getLinks()) {
+            System.out.println("Link: " + s);
+        }
+    }
 }
